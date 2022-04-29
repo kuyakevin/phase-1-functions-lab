38b26ecc25d5aaf7c3e0f8437522d19a89f901c0
Kevin Chaney <kevin_chaney@icloud.com>
2022-04-28 20:23:29 -0700

    commit tree 7a188bb69c82e3188b7c1417282b06fa32351a74

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    let blocks = Math.abs(distance-42);
+    return blocks
+}
+
+function distanceFromHqInFeet(distance) {
+    let feet = distanceFromHqInBlocks(distance) * 264;
+    return feet
+}
+
+function distanceTravelledInFeet(distance1, distance2) {
+    let feetTraveled = Math.abs((distance2 - distance1)) * 264;
+    return feetTraveled
+}
+
+function calculatesFarePrice(start, destination) {
+    let cost;
+    let block = Math.abs(destination - start);
+    if (block > 0 && block <= 1) {
+        return cost = 0
+    } 
+    let fareDistance = block * 264 ;
+    if (fareDistance > 400 && fareDistance <= 2000) {
+        cost = (fareDistance - 400) * 0.02;
+        return cost
+    } else if (fareDistance > 2000 && fareDistance <= 2500) {
+        return cost = 25
+    } else if (fareDistance > 2500) {
+        return 'cannot travel that far'
+    }
+}
